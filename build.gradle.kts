1a1cd0747090a63e9521dec02750f8f935328609
Ralf Stuckert <ralf.stuckert@compeople.de>
2021-08-01 19:48:22 +0200

    force cache save

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+//
+
 plugins {
 	id("org.springframework.boot") version "2.5.3"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
